b5b3a3acabb182ff0233120ed87ac1059f08a3a3
alfsuace <alfonso.suaace@educa.jcyl.es>
2023-09-27 15:24:30 +0200

    project start

---
+++ kotlin/com/iesam/huellaskotlintrainning/Main.kt
@@ -0,0 +1,17 @@
+package com.iesam.huellaskotlintrainning
+
+fun main(){
+    //Crea un caso de uso para cada uno de los métodos que existen en el repositorio.
+
+    fun example01(){
+        //Caso de Uso para getAllOk()
+    }
+
+    fun example02(){
+        //Caso de Uso para getAllError()
+    }
+
+    fun example03(){
+        //Caso de Uso para getAllError2()
+    }
+}
